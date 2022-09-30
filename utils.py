dba4303b6154679a48321b4769df159fb89d454a
William <william.meredith@gmail.com>
2022-09-30 17:32:58 +0200

    version 00

--- utils.py
+++ utils.py
@@ -1,2 +1,77 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
+import os
+from pytube import YouTube
+import streamlit as st
+import requests
+from time import sleep
 
+ai_auth_key="fc18a8029ffd47faa4e5b972b591ba1b"
+upload_endpoint = 'https://api.assemblyai.com/v2/upload'
+transcript_endpoint = 'https://api.assemblyai.com/v2/transcript'
+
+headers = {
+    'authorization' : ai_auth_key,
+    'content_type' : 'application/json'
+    }
+
+@st.cache()
+def download_audio(vid_url) :
+    yt = YouTube(vid_url)
+    audio = yt.streams.filter(only_audio=True).first()
+    out = audio.download()
+    base, ext = os.path.splitext(out)
+    out_file = base + '.mp3'
+    os.rename(out, out_file)
+    return yt.title, out_file
+
+
+@st.cache()
+def submit_transcription(save_location) :
+    
+    def read_file(save_location, chunk_size=5242880):
+        with open(save_location, 'rb') as _file:
+            while True:
+                data = _file.read(chunk_size)
+                if not data:
+                    break
+                yield data
+
+    response = requests.post(upload_endpoint,
+                            headers=headers,
+                            data=read_file(save_location))
+    return response.json()['upload_url'] # URL to the uploaded file
+
+@st.cache()
+def do_transcription(ai_url):
+    transcript_task = {
+        'audio_url' : ai_url ,
+        'speaker_labels' : True 
+        }
+    response = requests.post(transcript_endpoint,
+                             headers=headers,
+                             json=transcript_task)
+
+    transcript_id = response.json()['id']
+    polling_endpoint = transcript_endpoint + '/' + transcript_id
+    
+    speakers_df = pd.DataFrame()
+    while True :
+        poll = requests.get(polling_endpoint, headers=headers)
+        status = poll.json()['status']
+        print(status)
+        print(poll)
+
+        if status == 'submitted' or status == 'processing' :
+            sleep(2)
+        elif status == 'complete' :
+            print(poll)
+            speakers = poll.json()['utterances']
+            print(speakers)
+            speakers_df = pd.DataFrame(speakers)
+            break
+        else :
+            print(poll)
+            break
+    return speakers_df
+        
--- vid.py
+++ vid.py
@@ -1,2 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import streamlit as st
+import numpy as np
+import pandas as pd
+from utils import *
+
+st.title("Video Transcript, version 0.1")
+st.caption("A demonstration of the best available products")
+
+scp_video_source = "https://s3-eu-west-1.amazonaws.com/connectedviews-vod01/406ff546-3bc6-43f0-8142-06dd768a873d/en/406ff546-3bc6-43f0-8142-06dd768a873d_1200.mp4"
+video_source = "https://www.youtube.com/watch?v=SCnnTvk7PTg"
+
+st.subheader("Malaysia joins Marrakesh, March 2022")
+st.video(video_source)
+
+vid_title, save_location = download_audio(video_source)
+st.header(vid_title)
+st.audio(save_location)
+
+ai_url = submit_transcription(save_location)
+speakers_df = pd.DataFrame()
+speakers_df = do_transcription(ai_url)
+
+st.dataframe(speakers_df)
+
+st.subheader("Index of Speakers")
+
+for index, row in speakers_df.iterrows() :
+    with st.expander(row['speaker']) :
+        st.write(row['text'])
+        st.button(row['start_time'])
